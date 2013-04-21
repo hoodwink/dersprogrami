466bd2735e3610e79292476a555d18d6b056096e
yasin şahan <sahanyasin@gmail.com>
2013-04-21 22:15:24 +0300

    enttymodel eklendi

--- dersprogrami/dersprogrami/Default.aspx.cs
+++ dersprogrami/dersprogrami/Default.aspx.cs
@@ -6,12 +6,41 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using Ext.Net;
 
+
 namespace dersprogrami
 {
     public partial class _Default : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                SyllabusDbEntities db;
+                using (db = new SyllabusDbEntities())
+                {
+
+                    //var fakulteler = db.fakultes.ToList();
+
+                    //foreach (var item in fakulteler)
+                    //{
+                    //    Response.Write(item.fakulte_name + "<br>");
+
+                    //}
+
+                    //DropDownList1.DataSource = fakulteler;
+                    //DropDownList1.DataTextField = "fakulte_name";
+                    //DropDownList1.DataValueField = "fakulte_id";
+                    //DropDownList1.DataBind();
+
+                    //var bolumler = db.bolums.ToList();
+                    //GridView1.DataSource = bolumler;
+                    //GridView1.DataBind();
+
+
+                }
+               
+
+            }
 
         }
     }
