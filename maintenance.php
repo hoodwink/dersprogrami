222f248150e8c86eb4465f80f572c16ccc1baef1
Mehmet Fatih Gültekin <gultekin.mehmetfatih@gmail.com>
2013-04-21 20:56:04 +0300

    ilk commit
    ilk commit test

---
+++ maintenance.php
@@ -0,0 +1,10 @@
+<?php
+/* Turkceye Ceviren eka7a - http://www.e-piksel.com */
+
+// Heading
+$_['heading_title']     = 'Sipariş Sistemi Şu Anda Kapalı';
+
+// Text
+$_['text_maintenance']  = 'Sipariş Sistemi Şu Anda Kapalı';
+$_['text_message']      = '<h1 style="text-align:center;">Çalışma saatleri dışında olduğumuz için sipariş sistemimiz şu anda kapalı.<br/>Haftanın her günü 11:30 - 01:30 saatleri arasında sizlere hizmet veriyoruz.<br/>Anlayışınız için teşekkür ederiz.</h1>';
+?>
